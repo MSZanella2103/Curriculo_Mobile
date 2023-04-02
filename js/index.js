d46a6012a5d6491e5ebc316f9752dafe540df454
MSZanella2103 <117287369+MSZanella2103@users.noreply.github.com>
2023-04-02 13:01:59 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-var btn = document.querySelector('.button');
-var description = document.querySelector('.show-or-hidden')
+var btn = document.querySelector('.button_sobremim');
+var description = document.querySelector('.show-or-hidden_sobremim')
 
 btn.addEventListener('click', function(){
     if(description.style.display === 'block'){
